719afe49d6d076b9e7e0682e99cc99254a5a2abd
Voxuel <Leo.Fridh@hotmail.com>
2022-10-16 18:31:11 +0200

    Solved day 2 challange

--- SmoothSentences/Program.cs
+++ SmoothSentences/Program.cs
@@ -20,6 +20,8 @@ namespace SmoothSentences
             var pos = positions.Select(int.Parse).ToList();
             int x = CalcNumberOfIncreases(pos);
             Console.WriteLine(x);
+            int threeWindowMeasurment = ThreeMeasurementWindows(pos);
+            Console.WriteLine(threeWindowMeasurment);
 
         }
         private static int CalcNumberOfIncreases(List<int> input)
@@ -30,6 +32,23 @@ namespace SmoothSentences
             {
                 if (input[i] > input[i - 1]) count++;
                 i++;
+            }
+            return count;
+        }
+        private static int ThreeMeasurementWindows(List<int> input)
+        {
+            int count = 0;
+            for (int i = 3; i < input.Count; i++)
+            {
+                int first = input[i - 1];
+                int second = input[i - 2];
+                int third = input[i - 3];
+                int current = first + second + third;
+                int _first = input[i];
+                int _second = input[i - 1];
+                int _third = input[i - 2];
+                int next = _first + _second + _third;
+                if (next > current) count++;
             }
             return count;
         }
